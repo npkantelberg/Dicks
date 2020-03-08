b790251452ca802445541943d9dc0793e02ed752
Nicholas Kantelberg <nicholaskantelberg@nicholass-mbp.home>
2020-03-07 22:16:32 -0600

    built the header and started working on the body of the homepage.

--- wordpress/wp-content/themes/dicks/index.php
+++ wordpress/wp-content/themes/dicks/index.php
@@ -3,7 +3,22 @@
   <!-- Add homepage content here -->
   <section class="hero-section">
     <div class="hero-container">
-      <img src="<?php bloginfo('template_url'); ?>/images/dicks-banner.jpg" alt="Dicks Drive-In hero image" class="main-hero">
+      <!-- <img src="<?php bloginfo('template_url'); ?>/images/dicks-banner.jpg" alt="Dicks Drive-In hero image" class="main-hero"> -->
+    </div>
+  </section>
+  <section class="about-section">
+    <div class="about-container">
+      <div class="about-box about-text">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ante massa, convallis et efficitur quis, ultrices a tellus. Nullam vehicula iaculis blandit. Vivamus at viverra felis. Donec porta ac neque vitae venenatis. Curabitur quis turpis risus. Suspendisse vestibulum sollicitudin nibh at rhoncus. Donec viverra egestas lorem sit amet cursus. Praesent tempor ultricies auctor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce auctor facilisis rhoncus. Donec bibendum, ante ut porta pulvinar, sapien lorem iaculis ligula, sed viverra lorem tortor maximus eros. Nulla ut nisi sed sem congue efficitur. Aliquam interdum id orci id efficitur. Pellentesque eu eleifend eros.
+        </p>
+        <p>
+          Aenean vitae odio porttitor, sollicitudin ex id, tempus felis. Aliquam ut pharetra nulla. Maecenas rhoncus, risus vel auctor rutrum, sem dolor porttitor est, in pretium ligula erat non eros. Curabitur mollis turpis a ex lacinia, nec sollicitudin leo interdum. Maecenas elementum luctus mi vel sollicitudin. Quisque malesuada neque eros. Nunc non magna tortor. Maecenas et nisi non odio accumsan elementum ut at sapien. Nunc viverra tellus massa, in malesuada enim vestibulum vel. 
+        </p>
+      </div>
+      <div class="about-box hours-text">
+
+      </div>
     </div>
   </section>
 </main>
